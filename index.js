d6f281a7dd4fa1797e7bdb116b863c467f0d013a
“Zuzana <zuzana.moskvic@gmail.com>
2025-08-04 16:25:41 +0200

    Iterations 1-3

--- index.js
+++ index.js
@@ -1,96 +1,63 @@
-// Iteration 1: Names and Input
-const hacker1 = "Zuzana"; 
-console.log("The driver's name is "+ hacker1);
+// Iteration 1 
+const hacker1 = "Adam";
+console.log("The driver's name is", hacker1);
 
-const hacker2 = "Adam"; 
-console.log("The navigator's name is "+ hacker2);
+const hacker2 = "Eve";
+console.log("The navigator's name is", hacker2);
 
-// Iteration 2: Conditionals
-    if (hacker1.length>hacker2.length) {
-        console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
-    }
-    else if (hacker1.length<hacker2.length) {
-        console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
-    }
-    else if (hacker1.length===hacker2.length) {
-        console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-    }
+// Iteration 2 
 
-// Iteration 3: Loops
-let string ="";
-for (let i=0; i<=hacker1.length-1; i++) {
-    string += (hacker1[i].toUpperCase())+ " ";
-}
-console.log(string);
+const nameLength = (hacker1, hacker2) => { 
+    if (hacker1.length === hacker2.length) {
+         console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+    } else if (hacker2.length > hacker1.length) {
+         console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+    } else if (hacker1.length > hacker2.length) {
+         console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+    } else {
+        console.log("Something is wrong!");
+}};
 
+nameLength(hacker1, hacker2);
 
-let string2 = "";
-for (let i=hacker2.length-1; i>=0; i--) {
-    string2 += (hacker2[i]);
-}
-console.log(string2);
-
-
-if (hacker1<hacker2) {
-    console.log("The driver's name goes first.");
+// Iteration 3.1
+const capitaliseAndSpaceString = (string) => {
+    let capitalisedSpacedString = "";
+    for (let i=0; i<string.length; i++) {
+            const newChar = string[i].toUpperCase() + " ";
+            capitalisedSpacedString += newChar;
+}   return console.log(capitalisedSpacedString);;
 }
 
-else if (hacker2<hacker1) {
-    console.log("Yo, the navigator goes first definitely.");
+capitaliseAndSpaceString(hacker1);
+
+// Iteration 3.2
+
+
+const reverseString = (string) => {
+    let reversedString = "";
+    for (let i=string.length-1; i>=0; i--) {
+        const reversedChar = string[i];
+        reversedString += reversedChar;
+} return console.log(reversedString);
 }
 
-else if (hacker1=== hacker2) {
-    console.log("What?! You both have the same name?");
-}
+reverseString(hacker2);
 
-// Iteration 4: Bonus 1
-let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel urna vitae quam sagittis efficitur vel non leo. Proin aliquet ultricies erat non dapibus. Donec ornare scelerisque porttitor. Morbi aliquet laoreet mauris fringilla ullamcorper. Vivamus volutpat, libero nec porttitor dapibus, libero risus mollis elit, eu lacinia magna tellus vitae ligula. Praesent laoreet nunc et sapien facilisis, ut varius nisi sagittis. Mauris semper, diam sit amet ultricies bibendum, magna dolor dapibus odio, nec ullamcorper nibh urna id tortor. Vestibulum sollicitudin lectus eget semper accumsan. Nunc scelerisque condimentum eleifend. Quisque sit amet sapien neque. Etiam tempor, arcu id rhoncus mollis, lacus nisi facilisis orci, sed porttitor mi turpis vitae dui. Suspendisse consequat purus sed elit suscipit, non pellentesque libero hendrerit. Phasellus dictum massa nec justo lobortis, vitae iaculis eros ultrices. Quisque sit amet leo suscipit, auctor felis sit amet, congue erat. Cras hendrerit erat ut tellus ultricies, eu eleifend tortor ullamcorper. Etiam a lectus ut ex elementum posuere sit amet non risus. Pellentesque ut quam id tortor consectetur eleifend. Proin velit enim, volutpat quis augue quis, laoreet fringilla quam. Integer hendrerit, ipsum placerat hendrerit hendrerit, lorem lorem vehicula ipsum, blandit mollis nisi dui et felis. Praesent odio dui, pulvinar et quam rhoncus, aliquet facilisis sapien. Fusce tincidunt posuere metus, quis auctor est maximus eu. Ut vel volutpat orci, ac vulputate sapien.Vivamus sodales elit a arcu placerat, quis tempus eros auctor. Mauris pellentesque pretium purus, eu ultrices odio auctor quis. Sed tortor nunc, pellentesque in leo aliquet, finibus accumsan massa. Aliquam et molestie erat, non vestibulum justo. Integer tincidunt et massa ac efficitur. Duis mi ex, hendrerit non mauris id, dignissim convallis nunc. Vestibulum et tempus diam. Aliquam urna elit, feugiat quis pretium ac, convallis nec erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. ";
-
-let wordCount =0;
-for (let i=0; i<longText.length; i++) {
-    if (longText[i]=== " ") {
-        wordCount++;
+// Iteration 3.3
+const lexicographicOrder = (hacker1, hacker2) => {
+    if (hacker1>hacker2) {
+        console.log("The driver's name goes first.");
+    } else if (hacker1<hacker2) {
+        console.log("Yo, the navigator goes first definitely.");
+    } else if (hacker1=hacker2) {
+        console.log("What?! You both have the same name?");
     }
 }
-console.log(`Number of words: ${wordCount}`);
 
-let etCount =0;
-for (let i=0; i<longText.length; i++) {
-    if (longText[i] + longText[i+1] === "et" && longText[i-1] === (" " || "." ) && longText[i+2] === (" " || "." )){
-        etCount++;
-    }
-    else if (longText[i] + longText[i+1] === "Et" && longText[i-1] === (" " || "." ) && longText[i+2] === (" " || "." )){
-        etCount++;
-    }
-}
-console.log(`Number of "et": ${etCount}`);
+lexicographicOrder(hacker1, hacker2);
 
-// Iteration 5: Bonus 2
-let phraseToCheck ="Was it a car or a cat I saw?";
+// Bonus 1
 
-// prepare the phrase: remove special characters first & make lowercase
-let phraseNoSpaces="";
-for (let i=0; i<phraseToCheck.length; i++) {
-    if (phraseToCheck.charCodeAt(i) <= 90 && phraseToCheck.charCodeAt(i) >= 65) {
-        phraseNoSpaces += phraseToCheck[i];}  
-    else if (phraseToCheck.charCodeAt(i) <= 122 && phraseToCheck.charCodeAt(i) >= 97) {
-        phraseNoSpaces += phraseToCheck[i];
-    }
-}
-let phraseLowercase=phraseNoSpaces.toLowerCase();
 
-// check if first char is the same as last one etc. 
-let palindromCount="";
-for (let j=0; j<phraseLowercase.length/2; j++) {
-    if (phraseLowercase[j] === phraseLowercase[phraseLowercase.length-1-j]) {
-        palindromCount++;
-    }
- }
 
-// now, write down the result of the analysis 
- if (palindromCount >= phraseLowercase.length/2) {
-    console.log(`Yes, the phrase "${phraseToCheck}" is a palindrom.`);
- }
- else {
-    console.log(`No, the phrase "${phraseToCheck}" is not a palindrom.`);;
- }
